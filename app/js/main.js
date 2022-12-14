4f326e7faadb0602d498b674d0a78ba7c80f0ba7
Anton Tur <ant.tur85@gmail.com>
2022-12-14 18:28:43 +0200

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
-$(function () {
+const slider = tns({
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	nav: true,
+	speed: 1200,
+	responsive: {
 
-})
+		320: {
+			nav: true
+		},
+		767: {
+			nav: false
+		}
+	}
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
